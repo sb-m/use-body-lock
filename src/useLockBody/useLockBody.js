defba281281c75b9d7474f9713bfaca425cc0a24
Chris Vince <chrisvince@gmail.com>
2020-08-05 10:46:41 -0400

    Updates to node_modules, static site compatability
    Updated node_modules. Added check to ensure window is defined before
    running useLayoutEffect for static site generators.

--- src/useLockBody/useLockBody.js
+++ src/useLockBody/useLockBody.js
@@ -4,10 +4,11 @@ const Hook = () => {
 	const [bodyScrollLocked, setBodyScrollLocked] = useState(false)
 
 	useLayoutEffect(() => {
+		if (typeof window === 'undefined') return
 		if (bodyScrollLocked) {
-			const originalStyle = window.getComputedStyle(document.body).overflow;
-			document.body.style.overflow = 'hidden';
-			return () => document.body.style.overflow = originalStyle;
+			const originalStyle = window.getComputedStyle(document.body).overflow
+			document.body.style.overflow = 'hidden'
+			return () => document.body.style.overflow = originalStyle
 		}
 	}, [bodyScrollLocked])
 
